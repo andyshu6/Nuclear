47fa42939ed210fb5d15e7f5c911e5df5b620c60
John Doe <john@sample.com>
2018-06-15 01:02:18 +0800

    获取数据库数据

--- Pandora/views.py
+++ Pandora/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import ToolInfo
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def tools_test(request):
+    toollist = ToolInfo.objects.all()
     return render(request,'tools_test.html')
