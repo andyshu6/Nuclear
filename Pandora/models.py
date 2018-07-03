b37ac3e0d1c86b62483fb44b8886a4c5824a63db
John Doe <john@sample.com>
2018-07-04 00:22:30 +0800

    获取数据库数据

--- Pandora/models.py
+++ Pandora/models.py
@@ -13,10 +13,13 @@ class ToolInfo(models.Model):
     tool_name = models.CharField(max_length=64)
     tool_desc = models.CharField(max_length=128,null=True,blank=True)
     tool_url = models.CharField(max_length=256)
-    tool_imgaddr = models.CharField(max_length=128,null=True,blank=True)
+    tool_imgaddr = models.CharField(max_length=128,default="tool.jpg")
     tool_heat = models.IntegerField(default=0)
-    tool_group = models.ManyToManyField(ToolGroupInfo, related_name="type")
+    tool_group = models.ManyToManyField(ToolGroupInfo)
 
     def __unicode__(self):
         return self.tool_name
+
+    def get_img_url(self):
+        return "/static/pandora/img/tool/" + self.tool_imgaddr
 
--- Pandora/views.py
+++ Pandora/views.py
@@ -6,5 +6,8 @@ def index(request):
     return render(request,'index.html')
 
 def tools_test(request):
-    toollist = ToolInfo.objects.all()
-    return render(request,'tools_test.html')
+    toollist = ToolInfo.objects.all().select_related('group_code')
+    for i in toollist:
+        name = i.name
+        type = i.tool_group.group_name
+    return render(request,'tools_test.html', {'toollist':toollist})
