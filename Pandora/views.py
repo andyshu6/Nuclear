8b9b6db873acf0ac70656e5213172f28f08a5f82
andyshu6 <289486572@qq.com>
2018-07-10 13:47:24 +0800

    html实现manytomanyfiled

--- Pandora/views.py
+++ Pandora/views.py
@@ -8,9 +8,11 @@ def index(request):
 
 def tools_test(request):
     toollist = ToolInfo.objects.all()
-    grouplist = {}
-    for i in toollist:
-        type = i.tool_group.all()[0].group_name
-        grouplist.append(type)
+    # for i in toollist:
+    #     name = i.tool_name
+    #     print(name)
+    #     # type = i.tool_group.all()[0].group_name
+    #     for j in i.ToolInfo_set.all():
+    #         print(j)
 
-    return render(request,'tools_test.html', {'toollist':toollist, 'grouplist':grouplist})
+    return render(request,'tools_test.html', {'toollist':toollist})
